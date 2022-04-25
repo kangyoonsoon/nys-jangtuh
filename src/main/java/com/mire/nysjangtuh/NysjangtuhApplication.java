6861acc285e891bb715b03190379e15be21e125e
ys <kangyoonsoon@gmail.com>
2022-04-25 12:00:17 -0400

    First commit

---
+++ src/main/java/com/mire/nysjangtuh/NysjangtuhApplication.java
@@ -0,0 +1,13 @@
+package com.mire.nysjangtuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NysjangtuhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NysjangtuhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mire/nysjangtuh/NysjangtuhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mire.nysjangtuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NysjangtuhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
