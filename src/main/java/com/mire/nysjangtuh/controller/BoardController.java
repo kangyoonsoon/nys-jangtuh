444e9822f0015fc4db83981d2c73e1ea76aa89ca
ys <kangyoonsoon@gmail.com>
2022-04-26 15:46:43 -0400

    Implements of JPA, setting up MariaDB

--- src/main/java/com/mire/nysjangtuh/controller/BoardController.java
+++ src/main/java/com/mire/nysjangtuh/controller/BoardController.java
@@ -1,17 +1,29 @@
 package com.mire.nysjangtuh.controller;
 
 
+import com.mire.nysjangtuh.model.Board;
+import com.mire.nysjangtuh.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("/list")
-    public String list(){
+    @Autowired
+    private BoardRepository boardRepository;
 
+    @GetMapping("/list")
+    public String list(Model model){
+
+        List<Board> boards = boardRepository.findAll();
+//        System.out.println(boards);
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 }
---
+++ src/main/java/com/mire/nysjangtuh/model/Board.java
@@ -0,0 +1,30 @@
+package com.mire.nysjangtuh.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long num;
+    private String title;
+    private String content;
+    private String area;
+    private String pic1;
+    private String pic2;
+    private String pic3;
+    private String pic4;
+    private String pic5;
+    private Date date;
+    private String user_id;
+
+
+}
