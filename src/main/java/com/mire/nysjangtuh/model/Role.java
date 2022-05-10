879fdc58c0236defb4b17dbd60ee4c7a6602fb1a
ys <kangyoonsoon@gmail.com>
2022-05-10 10:20:01 -0400

    Initial setting up spring security and login and register

---
+++ src/main/java/com/mire/nysjangtuh/model/Role.java
@@ -0,0 +1,21 @@
+package com.mire.nysjangtuh.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
---
+++ src/main/java/com/mire/nysjangtuh/service/UserService.java
@@ -0,0 +1,33 @@
+package com.mire.nysjangtuh.service;
+
+import com.mire.nysjangtuh.model.Role;
+import com.mire.nysjangtuh.model.User;
+import com.mire.nysjangtuh.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User save(User user) {
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setEnabled(true);
+
+        Role role = new Role();
+        role.setId(1);
+
+        user.getRoles().add(role);
+
+        return userRepository.save(user);
+    }
+
+}
